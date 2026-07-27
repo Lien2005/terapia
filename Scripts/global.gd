cf22cf9a0085702c50f286cb2cdd5ed6f549197c
Levi0502 <bodalevente8@gmail.com>
2026-07-27 18:32:23 +0200

    Atkuldom a laptopra

--- Scripts/global.gd
+++ Scripts/global.gd
@@ -1,7 +1,5 @@
 extends Node
 
-var is_fullscreen: bool = true
-
 func _ready() -> void:
 	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
 
@@ -9,8 +7,7 @@ func _input(event: InputEvent) -> void:
 	if event.is_action_pressed("exit"):
 		get_tree().quit()
 	if event.is_action_pressed("fullscreen"):
-		is_fullscreen = not is_fullscreen
-		if is_fullscreen:
+		if DisplayServer.window_get_mode() == DisplayServer.WINDOW_MODE_FULLSCREEN:
 			DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_WINDOWED)
 		else:
 			DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_FULLSCREEN)
